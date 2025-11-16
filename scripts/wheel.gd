4669f21d9b8f715a718b40f087fc27475f0bea19
Daniel89987 <mxsl003@icloud.com>
2025-11-16 12:51:06 -0800

    Added wheel movement

--- scripts/wheel.gd
+++ scripts/wheel.gd
@@ -1,60 +1,33 @@
-extends StaticBody3D
+@tool
+class_name XRToolsWheelPickable
+extends XRToolsPickable
 
-#Maximum rotation and rotation speed
-@export var max_rotation_degrees := 360.0
-@export var rotation_speed := 2.0
+@export var max_roll_speed : float = 3.0
+@export var allow_rolling : bool = true
 
-var grabbed := false
-var grab_hand: XRController3D = null
-var initial_grab_angle := 0.0
-var initial_wheel_angle := 0.0
+var original_y_position : float
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	input_ray_pickable = true
-	#pass # Replace with function body.
+func _ready():
+	super._ready()
+	original_y_position = global_position.y
 
-#Return rotation angle
-func get_current_rotation_angle() -> float:
-	return rotation.y
+func pick_up(by: Node3D):
+	if can_pick_up(by):
+		original_y_position = global_position.y
+		super.pick_up(by)
 
-#Called when the player grabs the wheel
-func _on_grab(hand: XRController3D):
-	if not grabbed:
-		grabbed = true
-		grab_hand = hand
+func _physics_process(delta):
+	if is_picked_up() and allow_rolling:
+		# Only constrain Y position to prevent falling through floor
+		# but allow X and Z movement for rolling
+		var current_pos = global_position
+		current_pos.y = original_y_position
+		global_position = current_pos
+		
+		# Limit maximum speed
+		if linear_velocity.length() > max_roll_speed:
+			linear_velocity = linear_velocity.normalized() * max_roll_speed
 
-	# Calculate initial angles
-	var hand_pos = global_transform.origin
-	var wheel_pos = global_transform.origin
-	var to_hand = hand_pos - wheel_pos
-	var local_to_hand = global_transform.basis.inverse() * to_hand
-
-	initial_grab_angle = atan2(local_to_hand.z, local_to_hand.x)
-	initial_wheel_angle = get_current_rotation_angle()
-
-#Called when player release the wheel
-func _on_release(hand: XRController3D):
-	if grabbed and grab_hand == hand:
-		grabbed = false
-		grab_hand = null
-
-#Called when wheel is being rotated by player
-func update_wheel_rotation():
-	var hand_pos = grab_hand.global_transform.origin
-	var wheel_pos = global_transform.origin
-	var to_hand = hand_pos - wheel_pos
-	var local_to_hand = global_transform.basis.inverse() * to_hand
-	var current_grab_angle = atan2(local_to_hand.z, local_to_hand.x)
-	var angle_difference = current_grab_angle - initial_grab_angle
-	
-	var target_angle = initial_wheel_angle + angle_difference * rotation_speed
-	target_angle = clamp(target_angle, -deg_to_rad(max_rotation_degrees), deg_to_rad(max_rotation_degrees))
-	
-	rotation.y = target_angle
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	#pass
-	if grabbed and grab_hand:
-		update_wheel_rotation()
+func let_go(by: Node3D, linear_vel: Vector3, angular_vel: Vector3):
+	# Allow natural rolling physics when released
+	super.let_go(by, linear_vel, angular_vel)
