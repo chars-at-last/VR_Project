0f81f55da4bfd4b82227d94be1820b6eea0d5539
chars-at-last <PPL_PIKE@protonmail.com>
2025-11-20 00:13:19 -0800

    Wheelchair Part 1: Wheelchair Model

---
+++ scripts/wheelchair/wheelchair_model.gd
@@ -0,0 +1,23 @@
+@icon("res://icon.svg")
+
+class_name WheelchairModel extends Node3D
+
+
+# Variables
+@onready var chair: Node3D = $Chair
+@onready var back_wheels: Node3D = $BackWheels
+@onready var front_wheels: Node3D = $FrontWheels
+
+@onready var back_wheel_left: Node3D = $BackWheels/BackWheelLeft
+@onready var back_wheel_right: Node3D = $BackWheels/BackWheelRight
+
+# Testing
+#func _physics_process(delta: float) -> void:
+	#var value := PI / 2 * delta
+	#rotate_wheels(value, 2 * value, value)
+
+# Rotate wheels, choose whether to rotate left / right back wheels or nah
+func rotate_wheels(amount_rad_front: float, amount_rad_back_left: float, amount_rad_back_right: float) -> void:
+	front_wheels.rotate_x(amount_rad_front)
+	back_wheel_left.rotate_x(amount_rad_back_left)
+	back_wheel_right.rotate_x(amount_rad_back_right)
