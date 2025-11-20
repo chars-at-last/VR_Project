0cae348ae93e980e86de8eb858e838c0649d1551
chars-at-last <PPL_PIKE@protonmail.com>
2025-11-20 12:30:29 -0800

    Wheelchair Movement 2.0: WHEELCHAIR CAN ROLLgit add scenes scripts!

--- scripts/wheelchair/wheelchair_avatar.gd
+++ scripts/wheelchair/wheelchair_avatar.gd
@@ -4,7 +4,7 @@ class_name WheelchairAvatar extends CharacterBody3D
 
 
 # Constants
-const DEFAULT_ROTATION_AMOUNT: float = PI
+const DEFAULT_ROTATION_AMOUNT: float = PI * 30
 
 # Variables
 @onready var model: WheelchairModel = $WheelchairModel
@@ -13,12 +13,21 @@ const DEFAULT_ROTATION_AMOUNT: float = PI
 
 # Testing
 func _physics_process(delta: float) -> void:
-	velocity = global_transform.basis.z * 400 * delta
+	velocity = global_transform.basis.z * 200 * delta
 	move_and_slide()
-	rotate_around_point(true, delta)
 	rotate_around_point(false, delta)
 
 # Rotate around point
 func rotate_around_point(positive: bool, delta: float) -> void:
-	self.rotate_y((1 if positive else -1) * rotation_amount * delta)
+	var mult: float
+	var rot_amount_delta: float = rotation_amount * delta
+	
+	if positive:
+		mult = 1
+		model.rotate_wheels_const(delta, 0, delta)
+	else:
+		model.rotate_wheels(delta, delta, 0)
+		mult = -1
+	
+	self.rotate_y(mult * rot_amount_delta * delta)
 	
--- scripts/wheelchair/wheelchair_model.gd
+++ scripts/wheelchair/wheelchair_model.gd
@@ -3,6 +3,9 @@
 class_name WheelchairModel extends Node3D
 
 
+# Constants
+const MODEL_WHEEL_ROTATION_DEFAULT: float = PI / 2
+
 # Variables
 @onready var chair: Node3D = $Chair
 @onready var back_wheels: Node3D = $BackWheels
@@ -25,3 +28,7 @@ func rotate_wheels(amount_rad_front: float, amount_rad_back_left: float, amount_
 # Rotate all by same amount
 func rotate_wheels_same(amount_rad: float) -> void:
 	return rotate_wheels(amount_rad, amount_rad, amount_rad)
+
+# Rotate wheels using constant
+func rotate_wheels_const(mult_front: float = 1, mult_back_left: float = 1, mult_back_right: float = 1) -> void:
+	rotate_wheels(MODEL_WHEEL_ROTATION_DEFAULT * mult_front, MODEL_WHEEL_ROTATION_DEFAULT * mult_back_left, MODEL_WHEEL_ROTATION_DEFAULT * mult_back_right)
