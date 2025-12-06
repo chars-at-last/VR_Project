dfc5c7d912386965944377adb629eb5185da87b3
chars-at-last <PPL_PIKE@protonmail.com>
2025-12-05 16:12:32 -0800

    Wheelchair 3.1: Initial Keyboard Movement & Physics

--- scripts/wheelchair/wheelchair_avatar.gd
+++ scripts/wheelchair/wheelchair_avatar.gd
@@ -5,28 +5,77 @@ class_name WheelchairAvatar extends CharacterBody3D
 
 # Constants
 const DEFAULT_ROTATION_AMOUNT: float = PI * 30
+const FRICTION: float = .1
 
 # Variables
 @onready var model: WheelchairModel = $WheelchairModel
 
 @export var rotation_amount: float = DEFAULT_ROTATION_AMOUNT
 
-# Testing
+var direction: int = 0
+
+# Keyboard input - FOR TESTING ONLY !!!
+func keyboard_input(delta) -> void:
+	var speed: float = 6.67
+	
+	if Input.is_action_pressed("wheelchair_left"):
+		movement(speed, true, delta)
+	elif Input.is_action_pressed("wheelchair_left_rev"):
+		movement(-speed, true, delta)
+		
+	if Input.is_action_pressed("wheelchair_right"):
+		movement(speed, false, delta)
+	elif Input.is_action_pressed("wheelchair_right_rev"):
+		movement(-speed, false, delta)
+		
+	if Input.is_action_pressed("wheelchair_left2"):
+		movement(speed, true, delta)
+	elif Input.is_action_pressed("wheelchar_left2_rev"):
+		movement(-speed, true, delta)
+		
+	if Input.is_action_pressed("wheelchair_right2"):
+		movement(speed, false, delta)
+	elif Input.is_action_pressed("wheelchair_right2_rev"):
+		movement(-speed, false, delta)
+		
+	if (Input.is_action_just_released("wheelchair_left") or Input.is_action_just_released("wheelchair_right") or Input.is_action_just_released("wheelchair_left_rev") or Input.is_action_just_released("wheelchair_right_rev")) and not (Input.is_action_pressed("wheelchair_left") or Input.is_action_pressed("wheelchair_right") or Input.is_action_pressed("wheelchair_left_rev") or Input.is_action_pressed("wheelchair_right_rev")):
+		velocity = Vector3.ZERO
+		direction = 0
+
 func _physics_process(delta: float) -> void:
-	velocity = global_transform.basis.z * 200 * delta
+	# Testing
+	#velocity = global_transform.basis.z * 200 * delta
+	#move_and_slide()
+	#rotate_around_point(false, delta)
+	
+	keyboard_input(delta)
+	
 	move_and_slide()
-	rotate_around_point(false, delta)
+	if velocity.length_squared() > 0:
+		#print(velocity.length())
+		only_move(direction * (velocity.length() - FRICTION))
+	
+# Movement
+func movement(speed: float, positive: bool, delta: float) -> void:
+	only_move(speed)
+	direction = sign(speed)
+	#move_and_slide()
+	rotate_around_point(positive, sign(speed), delta)
+
+# Only move, no rotation
+func only_move(speed: float) -> void:
+	velocity = global_transform.basis.z * speed
 
 # Rotate around point
-func rotate_around_point(positive: bool, delta: float) -> void:
+func rotate_around_point(positive: bool, direction_pos: float, delta: float) -> void:
 	var mult: float
 	var rot_amount_delta: float = rotation_amount * delta
 	
 	if positive:
 		mult = 1
-		model.rotate_wheels_const(delta, 0, delta)
+		model.rotate_wheels_const(delta, 0, delta, direction_pos)
 	else:
-		model.rotate_wheels(delta, delta, 0)
+		model.rotate_wheels(delta, delta, 0, direction_pos)
 		mult = -1
 	
 	self.rotate_y(mult * rot_amount_delta * delta)
--- scripts/wheelchair/wheelchair_model.gd
+++ scripts/wheelchair/wheelchair_model.gd
@@ -20,15 +20,15 @@ const MODEL_WHEEL_ROTATION_DEFAULT: float = PI / 2
 	#rotate_wheels(value, 2 * value, value)
 
 # Rotate wheels, choose whether to rotate left / right back wheels or nah
-func rotate_wheels(amount_rad_front: float, amount_rad_back_left: float, amount_rad_back_right: float) -> void:
-	front_wheels.rotate_x(amount_rad_front)
-	back_wheel_left.rotate_x(amount_rad_back_left)
-	back_wheel_right.rotate_x(amount_rad_back_right)
+func rotate_wheels(amount_rad_front: float, amount_rad_back_left: float, amount_rad_back_right: float, mult: float = 1) -> void:
+	front_wheels.rotate_x(amount_rad_front * mult)
+	back_wheel_left.rotate_x(amount_rad_back_left * mult)
+	back_wheel_right.rotate_x(amount_rad_back_right * mult)
 
 # Rotate all by same amount
-func rotate_wheels_same(amount_rad: float) -> void:
-	return rotate_wheels(amount_rad, amount_rad, amount_rad)
+func rotate_wheels_same(amount_rad: float, mult: float = 1) -> void:
+	return rotate_wheels(amount_rad, amount_rad, amount_rad, mult)
 
 # Rotate wheels using constant
-func rotate_wheels_const(mult_front: float = 1, mult_back_left: float = 1, mult_back_right: float = 1) -> void:
-	rotate_wheels(MODEL_WHEEL_ROTATION_DEFAULT * mult_front, MODEL_WHEEL_ROTATION_DEFAULT * mult_back_left, MODEL_WHEEL_ROTATION_DEFAULT * mult_back_right)
+func rotate_wheels_const(mult: float = 1, mult_front: float = 1, mult_back_left: float = 1, mult_back_right: float = 1) -> void:
+	rotate_wheels(MODEL_WHEEL_ROTATION_DEFAULT * mult_front, MODEL_WHEEL_ROTATION_DEFAULT * mult_back_left, MODEL_WHEEL_ROTATION_DEFAULT * mult_back_right, mult)
