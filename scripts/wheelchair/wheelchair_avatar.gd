bc1bde5db38d108085ec2202cd82b01d4bb3b205
chars-at-last <PPL_PIKE@protonmail.com>
2025-12-10 20:16:21 -0800

    Wheelchair Movement 4: Joystick Input

--- scripts/wheelchair/wheelchair_avatar.gd
+++ scripts/wheelchair/wheelchair_avatar.gd
@@ -7,6 +7,7 @@ class_name WheelchairAvatar extends CharacterBody3D
 const DEFAULT_ROTATION_AMOUNT: float = PI / 2
 const FRICTION: float = .1
 const ROTATION_FRICTION: float = 2
+const BASE_SPEED: float = 6.67
 
 ## Variables
 @onready var model: WheelchairModel = $WheelchairModel
@@ -16,6 +17,7 @@ const ROTATION_FRICTION: float = 2
 @export var rotation_amount: float = DEFAULT_ROTATION_AMOUNT
 
 @export var keyboard_testing: bool = false
+@export var joystick_input_enabled: bool = true
 
 # General variables
 var direction: int = 0
@@ -44,9 +46,9 @@ func reset_wheel_prev_pos(reset_right: bool = true, reset_left: bool = true) -> 
 	if reset_left:
 		left_wheel_hitb_prev_pos = left_wheel_hitb_starting_pos
 
-# Keyboard input - FOR TESTING ONLY !!!
-func keyboard_input(delta) -> void:
-	var speed: float = 6.67
+# Keyboard input - FOR TESTING ONLY !!!f
+func keyboard_input(delta: float) -> void:
+	var speed: float = BASE_SPEED
 	rotate_left_sign = 0
 	rotate_right_sign = 0
 	pre_velocity = 0
@@ -77,6 +79,32 @@ func keyboard_input(delta) -> void:
 	if (Input.is_action_just_released("wheelchair_left") or Input.is_action_just_released("wheelchair_right") or Input.is_action_just_released("wheelchair_left_rev") or Input.is_action_just_released("wheelchair_right_rev")) and not (Input.is_action_pressed("wheelchair_left") or Input.is_action_pressed("wheelchair_right") or Input.is_action_pressed("wheelchair_left_rev") or Input.is_action_pressed("wheelchair_right_rev")):
 		reset()
 		
+# Joystick movement
+func joystick_input(delta: float) -> void:
+	# Variables
+	var left_influence: float = Input.get_action_strength("wheelchair_left_stick_up") - Input.get_action_strength("wheelchair_left_stick_down")
+	var right_influence: float = Input.get_action_strength("wheelchair_right_stick_up") - Input.get_action_strength("wheelchair_right_stick_down")
+	var influence_active: bool
+	
+	rotate_left_sign = 0
+	rotate_right_sign = 0
+	pre_velocity = 0
+	influence_active = false
+	
+	#print(left_influence)
+	#print(right_influence)
+	
+	if not is_zero_approx(left_influence):
+		movement(left_influence * BASE_SPEED, true, delta, absf(left_influence))
+		influence_active = true
+		
+	if not is_zero_approx(right_influence):
+		movement(right_influence * BASE_SPEED, false, delta, absf(right_influence))
+		influence_active = true
+		
+	if influence_active:
+		only_move(pre_velocity)
+		
 # Resets some information
 func reset() -> void:
 	velocity = Vector3.ZERO
@@ -92,6 +120,9 @@ func _physics_process(delta: float) -> void:
 	## Testing 2 ##
 	if keyboard_testing:
 		keyboard_input(delta)
+		
+	if joystick_input_enabled:
+		joystick_input(delta)
 	
 	## Always-running movement funcitons ##
 	move_and_slide()
@@ -101,7 +132,7 @@ func _physics_process(delta: float) -> void:
 		only_move(direction * (velocity.length() - FRICTION))
 	
 # Movement
-func movement(speed: float, left_wheel: bool, delta: float) -> void:
+func movement(speed: float, left_wheel: bool, delta: float, rotate_mult: float = 1) -> void:
 	#only_move(speed)
 	pre_velocity += speed
 	direction = sign(speed)
@@ -111,7 +142,7 @@ func movement(speed: float, left_wheel: bool, delta: float) -> void:
 		rotate_right_sign = direction
 	
 	# Rotation calculation for compound rotations
-	rotation_vel_rad = (rotate_left_sign - rotate_right_sign) * DEFAULT_ROTATION_AMOUNT
+	rotation_vel_rad = (rotate_left_sign - rotate_right_sign) * DEFAULT_ROTATION_AMOUNT * rotate_mult
 
 # Only move, no rotation
 func only_move(speed: float) -> void:
